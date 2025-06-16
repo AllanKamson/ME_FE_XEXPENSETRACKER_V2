2a991713c19f931fc1ca389871dd66a307758146
Allan Kamson <allan.kamson0209@gmail.com>
2025-06-16 17:23:17 +0530

    Updated the pagination icons

--- src/components/ExpenseList/ExpenseList.jsx
+++ src/components/ExpenseList/ExpenseList.jsx
@@ -1,13 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import TransactionCard from '../TransactionCard/TransactionCard';
-
-const ChevronLeft = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-left"><path d="m15 18-6-6 6-6"></path></svg>
-);
-
-const ChevronRight = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-right"><path d="m9 18 6-6-6-6"></path></svg>
-);
+import { IoIosArrowRoundBack, IoIosArrowRoundForward } from "react-icons/io";
 
 
 const ExpenseList = ({ expenses, onDelete, onEdit }) => {
@@ -63,11 +56,11 @@ const ExpenseList = ({ expenses, onDelete, onEdit }) => {
       {totalPages > 1 && ( 
         <div className="pagination-wrapper">
           <button onClick={handlePrevPage} disabled={currentPage === 1}>
-            <ChevronLeft />
+            <IoIosArrowRoundBack />
           </button>
           <p>{currentPage}</p> 
           <button onClick={handleNextPage} disabled={currentPage === totalPages}>
-            <ChevronRight />
+            <IoIosArrowRoundForward />
           </button>
         </div>
       )}
